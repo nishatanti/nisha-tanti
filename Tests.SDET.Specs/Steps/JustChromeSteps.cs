5392d61a8212a54cd7a34f7f202e3a6e49e7e0ca
test <test@TEST-PC>
2020-09-21 14:17:40 +0530

    commit

---
+++ Tests.SDET.Specs/Steps/JustChromeSteps.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace Tests.SDET.Specs.Steps
+{
+    [Binding]
+    public sealed class JustChromeSteps
+    {
+        IWebDriver driver;
+
+        [Given(@"I am using Chrome")]
+        public void GivenIAmUsingChrome()
+        {
+            driver = new ChromeDriver();
+           
+        }
+
+        [When(@"I visit the JustChrome page")]
+        public void WhenIVisitTheJustChromePage()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com/chrome/");
+            driver.Manage().Window.Maximize();
+        }
+
+        [Then(@"I am shown the JustChrome page")]
+        public void ThenIAmShownTheJustChromePage()
+        {
+            
+        }
+
+        [Given(@"I am using Internet Explorer")]
+        public void GivenIAmUsingInternetExplorer()
+        {
+            
+        }
+
+        [Then(@"I am shown an error page")]
+        public void ThenIAmShownAnErrorPage()
+        {
+            
+        }
+
+    }
+}
---
+++ Tests.SDET/PageObject/JustChromeRepo.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.SDET.PageObject
+{
+    class JustChromeRepo
+    {
+        IWebDriver driver;
+
+        public JustChromeRepo(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+            this.driver = driver;
+        }
+    }
+}
